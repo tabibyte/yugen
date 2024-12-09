e35fcb318f30bb4c733d5844382bd79c0eafd72d
tabibyte <tabib.hasanzadeh@gmail.com>
2024-12-09 22:30:43 +0400

    fixed opening window twice

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,11 @@ import os
 
 app = create_app()
 
+def open_browser():
+    webbrowser.open('http://127.0.0.1:5000/')
+
 if __name__ == '__main__':
     if not os.environ.get('VSCODE_PID'):
-        webbrowser.open('http://127.0.0.1:5000/')
+        if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+            open_browser()
     app.run(debug=True)
